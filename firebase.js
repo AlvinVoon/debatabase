a45e8bd8c859189d53a31b28ecaa39e53b026396
AlvinVoon <62414477+AlvinVoon@users.noreply.github.com>
2026-03-23 19:09:04 +0800

    firebase_config

--- firebase.js
+++ firebase.js
@@ -1,6 +1,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-import { firebaseConfig } from './env.js';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCR8D82MjVqnflxArpOnOZpOEnThDZ0mdA",
+  authDomain: "debatabase-5981b.firebaseapp.com",
+  projectId: "debatabase-5981b",
+  storageBucket: "debatabase-5981b.firebasestorage.app",
+  messagingSenderId: "635437236368",
+  appId: "1:635437236368:web:802b2d1a84619ba64b9815",
+  measurementId: "G-DVPTZXD8D4"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
